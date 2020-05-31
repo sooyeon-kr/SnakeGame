6cd8bb0ab3996ac74d843e033ded0cfc50b4b880
anseongjun <dkstjdwns@kookmin.ac.kr>
2020-05-31 17:57:16 +0900

    Update Stage.h

--- Stage.h
+++ Stage.h
@@ -1,28 +1,28 @@
-// #ifndef __Stage__H__
-// #define __Stage__H__
-// enum class TileType{
-//     Blank,
-//     Wall,
-//     ImmuneWall
-// };
+#ifndef __Stage__H__
+#define __Stage__H__
+enum class TileType{
+    Blank,
+    Wall,
+    ImmuneWall
+};
 
-// class Stage
-// {
-//     //생성자
-// public:
-//     //메소드
-//     bool loadStage(char* filename);
-//     void printStage();
+class Stage
+{
+    //생성자
+public:
+    //메소드
+    bool loadStage(char* filename);
+    void printStage();
 
-//     //멤버변수
-// private:
-//     int row = -1;
-//     int col = -1;
-//     int **map = nullptr;
-// };
+    //멤버변수
+private:
+    int row = -1;
+    int col = -1;
+    int **map = nullptr;
+};
 
-// #endif
-//혹시몰라서 이전 부분은 남겨두겠습니다.
+#endif
+
 #ifndef __Stage__H__
 #define __Stage__H__
 enum class TileType{
