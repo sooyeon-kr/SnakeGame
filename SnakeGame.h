23fdacda3c4fb43c5e0009ca5138c0ca74fc0972
anseongjun <dkstjdwns@kookmin.ac.kr>
2020-06-02 22:47:00 +0900

    Update SnakeGame.h

--- SnakeGame.h
+++ SnakeGame.h
@@ -5,12 +5,13 @@ class SnakeGame{
 
 public:
     SnakeGame() = default;
-    ~SnakeGame() = default;   
+    ~SnakeGame() = default;
 
 public:
     void Init();
     void Play();
-    
+    bool CheckWall(int x, int y);
+
 
 private:
     Stage mStage;
