69c54f313490ab8a2fa4cd6867d07d82374182cb
anseongjun <dkstjdwns@kookmin.ac.kr>
2020-06-11 22:29:47 +0900

    Update Snake.h

--- Snake.h
+++ Snake.h
@@ -1,5 +1,6 @@
 #ifndef __Snake__
 #define __Snake__
+#include <vector>
 
 //방향 종류
 enum class Direction{
@@ -37,11 +38,13 @@ public:
 
     Direction GetCurDirection(){return head.Dir;}
     bool IsAlive(){return isLive;}
-    
+
 public:
+    std::vector<Position> savesp; //savesp는 이동하기 이전의 좌표를 기억하기 위한 배열입니다.
+    std::vector<Position> sp;   //sp는 뱀의 좌표 담는겁니다
     DPosition head;
     int length = 3; //뱀의 초기 길이
     bool isLive = true; //뱀이 죽었는지 살았는지 체크
 };
 
-#endif 
+#endif
